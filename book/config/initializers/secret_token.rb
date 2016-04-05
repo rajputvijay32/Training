0a5df1f9e7714dad080dbf15c489d4aa0f78e0f7
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-05 14:49:15 +0530

    created book ch5 - ch12

---
+++ book/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = 'ad99ae72e47562533ce00bd50331a5e3111e566b9bb4b2a1ec7dcfef880752194ba3977e9cc8ab461ab457d216549577a86db9bd4c9a050b832bbd1f29e1df1f'
