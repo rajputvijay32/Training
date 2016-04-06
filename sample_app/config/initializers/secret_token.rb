11e9ec7d1556a05b785ad9dd55ea69a3834aefde
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-06 11:26:50 +0530

    "added sample app"

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c4cfba0055d1cc3347468ff4f4665b2dd00a5089e33b398916c1a995c76810b100475891fdba7cbb34a8d2567d58d4320038593e0941cca832861de0cca86fc3'
