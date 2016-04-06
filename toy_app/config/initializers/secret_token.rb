27f664a3ac4a3893ef1734a6fac674cee129a4fc
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-06 08:51:31 +0530

    created new app toy_app

---
+++ toy_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = '48ef2ff140e17e950be3480156b3b53604d4425af95932e590cf52c221e885be8f43e2e70571f222625c7de8d58d86fbce0eaa6ed3c12538586306d09e924424'
