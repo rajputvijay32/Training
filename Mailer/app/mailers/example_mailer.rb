b7b479970e46ea75e1ce5655ec55df5d2f57e452
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-06 08:46:45 +0530

    changes pushed tp master

---
+++ Mailer/app/mailers/example_mailer.rb
@@ -0,0 +1,7 @@
+class ExampleMailer < ActionMailer::Base
+  default from: "from@example.com"
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
+end
---
+++ Mailer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailer::Application.config.secret_token = 'be29650514989cb8b836f47b82624aee48636d418c6f8ff1aa384b495839e10f3a9f5d9db710866eef9372ed9edd57eaf87bf1dd55a81d27b458ee998beab81f'
