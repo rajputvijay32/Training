58f8ec63bc28e60a8887c2c1dd29246a6d727beb
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-07 12:23:40 +0530

    depot web product listing

---
+++ depotweb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depotweb::Application.config.secret_token = '6fa778267c94286d27aecfce8b792a4df9f29d517f84835cf3a989a6d362bb60a9311caa799b09db3e9b23496ace60333e9020908904913db0f5fc214bf8ed00'
