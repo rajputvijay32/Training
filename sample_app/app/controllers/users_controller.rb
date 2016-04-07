87a91fce88bcbd4ba85a6f3caa42e5bcd596ae44
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-07 10:37:18 +0530

    updating showing and deleting users

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 
+  include AbstractController::Callbacks
+
+  def index
+    @users = User.all
+  end
+
   def show
-    @user = User.find(params[:id])
+    #@users = User.paginate(:page => params[:page], :per_page => 5)
+    @users = User.all
   end
 
   def new
@@ -19,15 +26,50 @@ class UsersController < ApplicationController
     end
   end
 
-  
+  def edit
+    @user = User.find(params[:id])
+  end
 
-
-private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
 end
