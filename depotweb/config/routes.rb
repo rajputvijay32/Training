d59e9861461b151ba675079ecda5d64e8b179c91
Sam Ruby <vijay.rajput@traveltriangle.com>
2016-04-07 15:18:06 +0530

    chapter 10

--- depotweb/config/routes.rb
+++ depotweb/config/routes.rb
@@ -1,6 +1,13 @@
 Depotweb::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
+
   resources :products
 
+  root :to => 'store#index', as: 'store'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
